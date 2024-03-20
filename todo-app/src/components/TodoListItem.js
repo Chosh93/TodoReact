909812f9196d1933159d0f8381aa12c581b172c5
Chosh93 <chosh930911@gmail.com>
2024-03-20 16:26:42 +0900

    삭제, 수정 모달 진행

--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,23 +1,50 @@
-import React from "react";
-import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from "react-icons/md";
+import React, { useState } from "react";
+import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline, MdCreate } from "react-icons/md";
 import "../style/TodoListItem.scss";
 import cn from "classnames";
+import TodoDelModal from "./TodoDelModal";
 
 const TodoListItem = ( {schedule, deleteSchedule, onToggle}) => {
     const {id, sc, checked} = schedule;
+    const [delModal, setDelModal] = useState(false);
+    const [updModal, setUpdModal] = useState(false);
+    const [inputUpdate, setInputdate] = useState("");
+
+    const handleUpdate = (e) => {
+        setInputdate(e.target.value);
+    };
+
+    const openDelModal = () => {
+        setDelModal(true);
+    };
+
+    const openUpdModal = () => {
+        setUpdModal(true);
+    };
+
+    const closeModal = () => {
+        setDelModal(false);
+        setUpdModal(false);
+    };
+
     const handleDelete = (id) => {
         deleteSchedule(id);
     };
-    console.log(schedule);
+
     return (
         <div className={"TodoListItem"}>
             <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
                 {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
                 <div className="text">{sc}</div>
             </div>
-            <div className="remove">
-                <MdRemoveCircleOutline onClick={() => handleDelete(id)}/>
+            <div className="update">
+                <MdCreate onClick={openUpdModal}/>
             </div>
+            <div className="remove">
+                <MdRemoveCircleOutline onClick={openDelModal}/>
+            </div>
+            <TodoDelModal open={delModal} close={closeModal} header="삭제" confirm={() => handleDelete(id)} type="confirm">삭제 하시겠습니까?</TodoDelModal>
+            {/* <TodoModal open={updModal} close={closeModal} header="수정하기" confirm={() => handleDelete(id)} type="confirm"></TodoModal> */}
         </div>
     )
 }
